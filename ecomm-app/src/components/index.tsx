bbfe749a3f57fad0dcdb35ef5a8412d871ce90b3
behrooz khan <kbalochkhan67@gmail.com>
2024-06-08 00:50:37 +0500

    added two more comp

--- ecomm-app/src/components/index.tsx
+++ ecomm-app/src/components/index.tsx
@@ -3,8 +3,10 @@ import { lazy } from 'react';
 const Navbar = lazy(() => import('./Header/Navbar'));
 const Logo = lazy(()=> import("./Header/Logo"));
 const Header = lazy(()=> import("./Header/Header"));
-
+const LanguageChanger =   lazy(()=> import("./Header/LanguageChanger"));
+const HeaderTop =   lazy(()=> import("./Header/HeaderTop"));
+const NavRight = lazy(()=> import("./Header/NavRight"));
 
 export {
-    Navbar,Logo,Header
+    Navbar,Logo,Header,LanguageChanger,HeaderTop,NavRight
 }
