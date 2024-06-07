236c18ba6f91d42846f8cd032ab0dfab69f2b785
behrooz khan <kbalochkhan67@gmail.com>
2024-06-08 00:51:06 +0500

    create interface Obj File Where necessary Interface Present

--- ecomm-app/src/Utlis/InterfaceObj.tsx
+++ ecomm-app/src/Utlis/InterfaceObj.tsx
@@ -1,3 +1,20 @@
 
 
 
+
+// Navbar Items Interface
+interface NavLink {
+    items: { name: string, id: string }[];
+}
+
+export const navbarItems: NavLink[] = [
+   {
+    items: [
+        { name: "Home", id: "home" },
+        { name: "Contact", id: "contact" },
+        { name: "About", id: "about" },
+        { name: "Sign Up", id: "sign up" },
+       ]
+   }
+]
+
