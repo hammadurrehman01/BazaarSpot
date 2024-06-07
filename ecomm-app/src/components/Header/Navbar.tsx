802d84f444c56486f52fbcb5cffabc9fabed6d9b
behrooz khan <kbalochkhan67@gmail.com>
2024-06-08 00:49:57 +0500

    Added Nav Items In Navbar

--- ecomm-app/src/components/Header/Navbar.tsx
+++ ecomm-app/src/components/Header/Navbar.tsx
@@ -1,4 +1,6 @@
-import { Logo } from "..";
+import { Link } from "react-router-dom";
+import { Logo, NavRight } from "../index";
+import { navbarItems } from "../../Utlis/InterfaceObj";
 
 
 const Navbar = () => {
@@ -6,6 +8,29 @@ const Navbar = () => {
     <nav>
       <div>
       <Logo/>
+      </div>
+      <div>
+      {
+        navbarItems.map((items)=>(
+          <ul>
+            {
+              items.items.map((navLink)=>(
+                <li key={navLink.id}>
+                  <Link to={navLink.id}>
+                  {
+                    navLink.name
+                  }
+                  </Link>
+                </li>
+              ))
+            }
+          </ul>
+        ))
+      }
+      </div>
+
+      <div>
+      <NavRight/>
       </div>
 
       
