a260e384461ca492545d08a52535dbe97a79ca5d
behrooz khan <kbalochkhan67@gmail.com>
2024-06-21 19:49:58 +0500

    minor chnages

--- ecomm-app/src/components/Header/NavRight.tsx
+++ ecomm-app/src/components/Header/NavRight.tsx
@@ -1,25 +1,61 @@
 import SearchIcon from '../../assets/images/searchIcon.svg';
 import WishList from '../../assets/images/HeartWishlist.svg';
+import ProfileIcon from '../../assets/images/userProfile.svg';
 import CartIcon from '../../assets/images/CartIcon.svg';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const NavRight = () => {
     return (
-        <div className='flex py-4 gap-8'>
-            <div className="relative flex items-center w-full max-w-lg bg-[var(--mainSecondaryLightWhite)] shadow-lg">
-                <input 
-                    type="text" 
-                    placeholder="What are you looking for?" 
-                    className="w-full py-2 pl-4 pr-12 border-none shadow-sm focus:outline-none"
+        // <div className='flex bg-red-500'>
+        //     <div className="relative flex items-center w-full max-w-lg bg-[var(--mainSecondaryLightWhite)] shadow-lg">
+        //         <input
+        //             type="text"
+        //             placeholder="What are you looking for?"
+        //             className="w-full py-2 pl-6 pr-12 border-none  focus:outline-none bg-[#f5f5f5] text-[17px]"
+        //         />
+        //         <Button className='bg-[var(--mainSecondaryLightWhite)]'>  
+        //             <img
+        //             src={SearchIcon}
+        //             alt="search icon"
+        //             className=""
+        //         />
+        //         </Button>
+        //     </div>
+        //     <div className="flex max-w-[250px]">
+
+        //         <Link to={'#'}><Button> <img src={WishList} alt="wishlist" width={32} /></Button></Link>
+        //         <Link to={'#'}><Button> <img src={CartIcon} alt="cart" width={32} /></Button></Link>
+
+        //         <Link to={'#'}><Button> <img src={ProfileIcon} alt="Profile" width={32} /></Button></Link>
+        //     </div>
+        // </div>
+        <div className="flex items-center">
+            {/* <NavRight/> */}
+            <div className="flex relative bg-[var(--mainSecondaryLightWhite)] shadow-lg">
+                <input
+                    type="text"
+                    placeholder="What are you looking for?"
+                    className="py-2 pl-6 pr-2 border-none  focus:outline-none bg-[#f5f5f5] text-[17px]"
                 />
-                <img 
-                    src={SearchIcon} 
-                    alt="search icon" 
-                    className="absolute right-4 top-1/2 transform -translate-y-1/2 w-5 h-5 cursor-pointer"
-                />
+                <Button className='bg-[var(--mainSecondaryLightWhite)]'>
+                    <img
+                        src={SearchIcon}
+                        alt="search icon"
+                        className="absolute"
+                    />
+                </Button>
             </div>
-            <div className="flex items-center gap-4">
-                <img src={WishList} alt="wishlist" width={50} />
-                <img src={CartIcon} alt="cart" width={50} />
+            <div className="flex items-center ml-4">
+                <div className='relative '>
+                    {/* <span className=' absolute right-[45%] rounded-full bg-red-500  text-[var(--mainSecondaryRedish)] font-bold'>4</span> */}
+                    <span className='absolute p-2 right-[20%] rounded-full bg-red-500 text-white font-bold border-2 border-white flex items-center justify-center w-[22px] h-[22px]'>
+                        4
+                    </span>
+                    <Link to={'#'}> <img src={WishList} alt="wishlist" width={33} className="mr-4" /></Link>
+                </div>
+                <Link to={'#'}> <img src={CartIcon} alt="wishlist" width={33} /></Link>
+                <Link to={'#'}> <img src={ProfileIcon} alt="wishlist" width={33} className="ml-4" /></Link>
             </div>
         </div>
     );
--- ecomm-app/src/components/Header/Navbar.tsx
+++ ecomm-app/src/components/Header/Navbar.tsx
@@ -1,39 +1,31 @@
 import { Link } from "react-router-dom";
 import { Logo, NavRight } from "../index";
 import { navbarItems } from "../../Utlis/InterfaceObj";
-
-
+import { Button } from "@mui/material";
 const Navbar = () => {
   return (
-    <nav className="flex justify-evenly items-center w-full border-b-2 ">
+    <nav
+      className="flex items-center justify-evenly border-b-2"
+    >
       <div>
-      <Logo/>
+        <Link to={'/'}><Logo /></Link>
       </div>
       <div className="">
-      {
-        navbarItems.map((items)=>(
-          <ul className="flex gap-10">
-            {
-              items.items.map((navLink)=>(
-                <li key={navLink.id} className="text-[1.100rem]">
-                  <Link to={navLink.id}>
-                  {
-                    navLink.name
-                  }
-                  </Link>
-                </li>
-              ))
-            }
-          </ul>
-        ))
-      }
+        {
+          navbarItems.map((items) => (
+            <ul className="flex gap-2 ml-8">
+              {
+                items.items.map((link) => (
+                  <>
+                    <li key={link.id}><Link to={link.id} ><Button className="text-[1.200rem]" style={{color: 'var(--mainTextBlack)'}}>{link.name}</Button></Link></li>
+                  </>
+                ))
+              }
+            </ul>
+          ))
+        }
       </div>
-
-      <div>
-      <NavRight/>
-      </div>
-
-      
+   <NavRight/>
     </nav>
   )
 }
